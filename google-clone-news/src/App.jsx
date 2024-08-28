0d2c79bfcad31868093056962af28697da4f00d0
nadhifa14 <nadhifaqila@gmail.com>
2024-08-28 14:18:57 +0700

    add menubar

--- google-clone-news/src/App.jsx
+++ google-clone-news/src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/Navbar"
+import Menubar from "./components/Menubar"
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
+      <Menubar />
     </div>
+
   )
 }
 
